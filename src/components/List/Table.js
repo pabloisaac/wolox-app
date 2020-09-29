f95180d1ceba8fbe517a1a5ee8b6049db49821e7
Pablo Isaac <juan.isaac@mercadolibre.com>
2020-09-29 19:27:05 -0300

    the ordered list and the number of technologies are displayed

--- src/components/List/Table.js
+++ src/components/List/Table.js
@@ -38,8 +38,8 @@ const Table = () => {
       _.forEach(item, (value, key) => {
         if (key === cell) {
           cells.push(
-            <div className="css-content cell-size" key={index+value}>
-              <img src={value} alt={value + index} className="img-content"/>
+            <div className="css-content cell-size" key={index + value}>
+              <img src={value} alt={value + index} className="img-content" />
             </div>
           );
         }
@@ -48,42 +48,62 @@ const Table = () => {
     return cells;
   };
 
+  const onSort = (e) =>{
+    if(e.target.name === "asc"){
+      let order = _.orderBy(state.list_search, ['tech'], ['asc']);
+      dispatch(setDataSearch(order));
+    }
+    if(e.target.name === "desc"){
+      let order = _.orderBy(state.list_search, ['tech'], ['desc']);
+      dispatch(setDataSearch(order));
+    }
+  }
+
   return (
-    <div className="responsive-table">
-      <section className="css-section-tech">
-        <h2 className="css-content header-size">Tecnologia</h2>
-        {buildCell("tech")}
-      </section>
+    <div>
+      <div className="sort-table">
+        <input type="submit" name="asc" value="ASC" onClick={ e => onSort(e)}/>
+        <input type="submit" name="desc" value="DESC" onClick={ e => onSort(e)}/>
+      </div>
+      <div className="responsive-table">
+        <section className="css-section-tech">
+          <h2 className="css-content header-size">Tecnologia</h2>
+          {buildCell("tech")}
+        </section>
 
-      <section className="css-section-year">
-        <h2 className="css-content header-size">Año</h2>
-        {buildCell("year")}
-      </section>
+        <section className="css-section-year">
+          <h2 className="css-content header-size">Año</h2>
+          {buildCell("year")}
+        </section>
 
-      <section className="css-section-author">
-        <h2 className="css-content header-size">Autor</h2>
-        {buildCell("author")}
-      </section>
+        <section className="css-section-author">
+          <h2 className="css-content header-size">Autor</h2>
+          {buildCell("author")}
+        </section>
 
-      <section className="css-section-license">
-        <h2 className="css-content header-size">Licencia</h2>
-        {buildCell("license")}
-      </section>
+        <section className="css-section-license">
+          <h2 className="css-content header-size">Licencia</h2>
+          {buildCell("license")}
+        </section>
 
-      <section className="css-section-language">
-        <h2 className="css-content header-size">Lenguaje</h2>
-        {buildCell("language")}
-      </section>
+        <section className="css-section-language">
+          <h2 className="css-content header-size">Lenguaje</h2>
+          {buildCell("language")}
+        </section>
 
-      <section className="css-section-type">
-        <h2 className="css-content header-size">Tipo</h2>
-        {buildCell("type")}
-      </section>
+        <section className="css-section-type">
+          <h2 className="css-content header-size">Tipo</h2>
+          {buildCell("type")}
+        </section>
 
-      <section className="css-section-logo">
-        <h2 className="css-content header-size">Logo</h2>
-        {buildCellLogo("logo")}
-      </section>
+        <section className="css-section-logo">
+          <h2 className="css-content header-size">Logo</h2>
+          {buildCellLogo("logo")}
+        </section>
+      </div>
+      <div className="total-table">
+        <label>Total de tecnlogias: {_.size(state.list_search)}</label>
+      </div>
     </div>
   );
 };
--- src/pages/NoPageFound.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const NonExistentPage = () => {
-    return (
-      <div>
-        <h1>404</h1> <p>Page doesn't exist</p>
-      </div>
-    )
-}
-export default NonExistentPage;
